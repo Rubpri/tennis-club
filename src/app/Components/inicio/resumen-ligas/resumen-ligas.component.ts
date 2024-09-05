580d1801f053aada962d31cc8d4ce4d729cabd32
Rubpri <rubenprietoserrano@gmail.com>
2024-09-05 20:14:08 +0200

    añadido logica para cambiar de ultimos resultados

--- src/app/Components/inicio/resumen-ligas/resumen-ligas.component.ts
+++ src/app/Components/inicio/resumen-ligas/resumen-ligas.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resumen-ligas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resumen-ligas.component.html',
   styleUrl: './resumen-ligas.component.css'
 })
 export class ResumenLigasComponent {
+
+  public currentCard: number;
+
+  constructor() {
+    this.currentCard = 1;
+  }
 
 }
